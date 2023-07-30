8fc4cfbd2d49b3a63a1127825483067fc76ec4cc
JCzyszczon <jakub.czyszczon1@gmail.com>
2023-07-30 23:04:13 +0200

    update

--- app/components/title.jsx
+++ app/components/title.jsx
@@ -7,10 +7,10 @@ export default function Title() {
         <Image src={MauveLogo} alt='Logo' className='w-[180px] h-auto'/>
         <h1 className='md:text-2xl sm:text-xl text-lg font-theSeasons font-light'>Profesjonalny makijaż na wyjątkowe okazje<br/>Perfekcyjne brwi na codzień</h1>
         <span className='px-5'>
-          <p className='md:text-base sm:text-sm text-[13px] font-theSeasons'>Zadbaj o oprawę swoich oczu i czuj się piękniejsza na codzień bez tracenia czasu na malowanie brwi!</p>
-          <p className='md:text-base sm:text-sm text-[13px] font-theSeasons'>Skorzystaj z najbardziej naturalnie wyglądającego makijażu permanentnego brwi metodą włoskową.</p>
+          <p className='md:text-base sm:text-sm text-[14px] font-theSeasons'>Zadbaj o oprawę swoich oczu i czuj się piękniejsza na codzień bez tracenia czasu na malowanie brwi!</p>
+          <p className='md:text-base sm:text-sm text-[14px] font-theSeasons'>Skorzystaj z najbardziej naturalnie wyglądającego makijażu permanentnego brwi metodą włoskową.</p>
         </span>
-        <p className='md:text-base sm:text-sm text-[13px] font-theSeasons px-5'>Poczuj się wyjątkowo w makijażu okazjonalnym idealnie dopasowanym do Twojej naturalnej urody.</p>
+        <p className='md:text-base sm:text-sm text-[14px] font-theSeasons px-5'>Poczuj się wyjątkowo w makijażu okazjonalnym idealnie dopasowanym do Twojej naturalnej urody.</p>
     </section>
   )
 }
