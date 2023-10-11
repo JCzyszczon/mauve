d1eb328ab6ec25afcf181f5205a7da9b114d811c
JCzyszczon <jakub.czyszczon1@gmail.com>
2023-10-11 21:52:48 +0200

    update

--- app/components/facebookMsg.tsx
+++ app/components/facebookMsg.tsx
@@ -4,7 +4,7 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 
 const FacebookMsg = () => {
     return (
-        <FacebookProvider appId="704009028267866" chatSupport>
+        <FacebookProvider appId="704009028267866" language='pl_PL' chatSupport>
           <CustomChat pageId="2150231035264148" themeColor='#cdbebf' minimized={true}/>
         </FacebookProvider>    
     );
